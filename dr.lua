7529db9128a1c33f997b1b35608dd9fd59c2e753
Hima <99522549+Levi4Chan@users.noreply.github.com>
2025-05-06 10:15:23 +0800

    Update dr.lua

--- dr.lua
+++ dr.lua
@@ -1,9 +1,11 @@
 script_key="wFbDSAywfobaCIGkaKixLnibwcJHFbgL";
 (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
 
-queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
-game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
-    if State == Enum.TeleportState.Started then
-        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Levi4Chan/4MyEyesOnly/refs/heads/main/dr.lua'))()")
-    end
+local queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+local Players = game:GetService("Players")
+
+Players.LocalPlayer.OnTeleport:Connect(function(State)
+	if queueteleport then
+		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Levi4Chan/4MyEyesOnly/refs/heads/main/dr.lua'))()")
+	end
 end)
