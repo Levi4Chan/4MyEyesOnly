0034a2b63111a3aadae135f16dd8b34cd3e2142b
Hima <99522549+Levi4Chan@users.noreply.github.com>
2025-05-06 09:58:19 +0800

    Update dr.lua

--- dr.lua
+++ dr.lua
@@ -1,2 +1,5 @@
 script_key="wFbDSAywfobaCIGkaKixLnibwcJHFbgL";
 (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+queueteleport("https://raw.githubusercontent.com/Levi4Chan/4MyEyesOnly/refs/heads/main/dr.lua")
